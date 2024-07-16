e7de0ab84ea08514730c33ede5a5ad0f0693c14f
EC2 Default User <ec2-user@ip-172-31-39-110.ec2.internal>
2024-07-16 02:52:45 +0000

    save1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -24,7 +24,7 @@ class Customer < ApplicationRecord
   validates :kana_first_name, :kana_last_name,
   format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "カタカナで入力して下さい。"}
   
-  #退会機能
+  #退会済みかどうか確認する
   def active_for_authentication?
     super && (self.is_active == false)
   end
