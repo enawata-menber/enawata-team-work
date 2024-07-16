9017b47e4c0e5b1feb7d388645d6a8cb51c4e8bf
EC2 Default User <ec2-user@ip-172-31-38-184.ap-northeast-1.compute.internal>
2024-07-16 07:54:09 +0000

    add:Gemfile_kaminari,item_controller,index.html.erb

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,10 @@
 class Public::ItemsController < ApplicationController
+    
+    def index
+        @items = Item.page(params[:page]).per(8)
+        @item_count = Item.count
+    end
+    
+    def show
+    end
 end
