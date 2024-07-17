c21d3708e08692cbe1a5a9b834c31f6853c6afb6
harunaendo <harunaendo423@gmail.com>
2024-07-17 05:32:34 +0000

    save

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,14 +1,21 @@
 class Admin::CustomersController < ApplicationController
     def index
+        @customers = Customer.all
     end
     
     def show
+        #@customer = Customer.find(params[:id])
     end
    
     def edit
+        #@customer = Customer.find(params[:id])
     end
     
     def update
+    end
+    
+    private
+    def customer_params
     end
 end
 
