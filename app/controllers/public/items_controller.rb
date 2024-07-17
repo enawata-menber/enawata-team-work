5e395a666d8a730a917c8377288e147c2900dd43
EC2 Default User <ec2-user@ip-172-31-38-184.ap-northeast-1.compute.internal>
2024-07-17 13:41:59 +0000

    add:ジャンル検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,30 @@
 class Public::ItemsController < ApplicationController
     
     def index
-        @items = Item.page(params[:page]).per(8)
-        @item_count = Item.count
+        #ジャンルIDが指定されている場合にそのジャンルに属する商品を取得し
+      if params[:genre_id].present?
+          @genres = Genre.all
+          @genre = Genre.find_by(id: params[:genre_id])
+          @items = Item.where(genre_id: @genre&.id).page(params[:page]).per(8)
+          @item_count = @items.total_count
+      else #指定されていない場合は全商品を取得
+          @items = Item.all.page(params[:page]).per(8)
+           @item_count = @items.total_count
+      end
     end
     
     def show
+    end
+    
+    #ジャンル検索
+    def search
+         # ジャンルIDに基づいた商品のクエリを一度だけ実行
+    items_query = Item.where(genre_id: params[:genre_id])
+    @items = items_query.page(params[:page]).per(8) # 例としてページあたり10項目を表示
+    @item_count = items_query.count
+
+    @genres = Genre.where(valid_invalid_status: 0) # 有効なジャンルのみを取得
+
+    render 'index' # 指定したビューファイルを明示的に表示
     end
 end
