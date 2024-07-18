c388b63ba0cc5dbe82a052d620c3cff8d8682b2d
EC2 Default User <ec2-user@ip-172-31-46-14.ap-northeast-1.compute.internal>
2024-07-18 05:44:50 +0000

    先にジャンル機能を実装します

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,35 +1,38 @@
 class Admin::ItemsController < ApplicationController
-   def index
-   end
-
-   def new
-    @item = Item.new
-   end
-  
-   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to admin_items_path
-    else
-      render :new
+    def index
+      @items = Item.all
     end
-   end
+
+    def new
+    @item = Item.new
+    end
+  
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+        redirect_to admin_items_path(@item)
+      else
+        render :new
+      end
+    end
 
    def show
      @item = Item.find(params[:id])
    end
 
-#   def edit
-#      @item = Item.find(params[:id])
-#   end
+   def edit
+      @item = Item.find(params[:id])
+   end
   
-#   def update
-#   end  
+   def update
+      @item = Item.find(params[:id])
+        
+   end  
 
 private
-def item_params
-
-  params.require(:item).permit(:name, :introduction, :price, :is_active, :image, :genre_id)
-
-end
+    def item_params
+    
+      params.require(:item).permit(:name, :introduction, :price, :is_active, :image, :genre_id)
+    
+    end
 end
