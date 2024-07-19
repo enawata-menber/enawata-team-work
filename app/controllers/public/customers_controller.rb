bbcfa8e6c4acfabf2647aa28992d9b7eaf8d234a
EC2 Default User <ec2-user@ip-172-31-39-110.ec2.internal>
2024-07-19 06:09:47 +0000

    public-c-login+mypage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,27 @@
 class Public::CustomersController < ApplicationController
+  
+  def show
+    @customer = current_customer
+  end
+  
+  def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = current_customer
+    if@customer.update(current_customer)
+       redirect_to public_customer_my_page_path
+    else
+       render :edit
+    end
+  end
+  
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number)
+  end
+
 end
