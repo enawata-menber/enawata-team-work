dbf88e00b4e6b981e95c690108204ceddb006cdd
EC2 Default User <ec2-user@ip-172-31-39-110.ec2.internal>
2024-07-19 06:02:12 +0000

    public-c-login-add

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -7,7 +7,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   private
   
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
 
   # GET /resource/sign_up
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2024_07_15_073229) do
 
   create_table "addresses", force: :cascade do |t|
@@ -52,7 +51,7 @@ ActiveRecord::Schema.define(version: 2024_07_15_073229) do
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_apasswordt"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
