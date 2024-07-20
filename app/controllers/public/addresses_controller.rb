a014c27976bcb9813a76a8876328b9ce67f342b6
EC2 Default User <ec2-user@ip-172-31-39-110.ec2.internal>
2024-07-20 11:27:20 +0000

    public-c-login-oddress-orders-add

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,22 +1,43 @@
 class Public::AddressesController < ApplicationController
+  before_action :authenticate_customer!
   
   def index
-    
+    @addresses = current_customer.addresses
+    @address = Address.new
   end
   
-  def show
-    
+  def edit
+    @address = Address.find(params[:id])
   end
   
   def create
-    
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      flash.now[:notice] = "新規配送先を登録しました"
+      redirect_to public_addresses_path
+    end
   end
   
   def updeate
-    
+    address = Address.find(params[:id])
+    address.update(address_params)
+    flash.now[:alert] = "変更内容を登録しました"
+    redirect_to public_addresses_path
   end
   
   def destroy
-    
+    address = Address.find(params[:id])
+    address = address.destroy
+    flash.now[:alert] = "配送先を削除しました"
+    redirect_to public_addresses_path
   end
+  
+   private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+  
+  
 end
