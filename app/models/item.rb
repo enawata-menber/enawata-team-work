6163ed5fd043f5a1d791ec8f27ac30b4c5315de9
EC2 Default User <ec2-user@ip-172-31-46-14.ap-northeast-1.compute.internal>
2024-07-22 04:57:05 +0000

    fic image_tag

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,11 @@ class Item < ApplicationRecord
   has_one_attached :image
   
   def get_proauct_image(width, height)
-  # 画像の取得処理を実装する
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  end
+  image.variant(resize_to_limit: [width, height]).processed
   end
   
   #genre_id,name,price,の欠損確認
@@ -17,5 +21,4 @@ class Item < ApplicationRecord
   validates :genre_id, :name, :price, presence: true
 	validates :introduction, length: {maximum: 200}
 	validates :price, numericality: {only_integer: true}
-	
 end
