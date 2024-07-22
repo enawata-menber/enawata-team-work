a3b22cc870ef7d11f26b09934d78440828beb874
EC2 Default User <ec2-user@ip-172-31-38-184.ap-northeast-1.compute.internal>
2024-07-22 12:27:11 +0000

    fix:二重記述コメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -38,8 +38,8 @@ Rails.application.routes.draw do
     post '/customers/sign_in', to: 'sessions#create',as: 'customer_session'
     delete '/customers/sign_out',to: 'sessions#destroy',as: 'destroy_customer_session'
     #public/customers
-    get '/custommers/my_page', to: 'customers#show', as: 'customer_my_page'
-    get '//customers/information/edit', to: 'customers#edit', as: 'edit_customer_information'
+    #get '/custommers/my_page', to: 'customers#show', as: 'customer_my_page' #重複している
+    #get '//customers/information/edit', to: 'customers#edit', as: 'edit_customer_information'#重複している
     patch '/customers/information', to: 'customers#update', as:  'update_customer_information'
     get '/customers/unsubscribe', to: 'customers#unsubscribe',as: 'customer_unsubscribe'
     patch '/customers/withdraw',to: 'customers#withdraw', as: 'customer_withdraw'
