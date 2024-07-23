362cc69399b443404ca08c2d871e0cff75518994
harunaendo <harunaendo423@gmail.com>
2024-07-23 07:47:23 +0000

    save

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,6 @@
 class Admin::OrdersController < ApplicationController
+    before_action :authenticate_admin!
+
     def index
         @orders = Order.all
     end
@@ -6,5 +8,19 @@ class Admin::OrdersController < ApplicationController
     def show
         @order = Order.find(params[:id])
         @order_details = @order.order_details
+    end
+    
+    def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    @order_details = @order.order_details
+
+    if @order.status == "入金確認"
+      @order_details.each do |order_detail|
+        order_detail.make_status = "製作待ち"
+        order_detail.save
+      end
+    end
+    redirect_to admin_order_path
     end
 end
