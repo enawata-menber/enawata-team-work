8eca4ed53fc1b51e62a9c2931aede261c1103f1e
harunaendo <harunaendo423@gmail.com>
2024-07-23 08:41:10 +0000

    save

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-##before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -13,46 +13,17 @@ class Admin::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  #DELETE /resource/sign_out
+  def destroy
+    #サインアウト処理
+     reset_session
+    redirect_to admin_root_path, notice: 'Signed out successfully.'
+  end
 
-   protected
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
-#  private
-  
-#  def customer_state
-#    @customer = Customer.find_by(email: params[:customer][:email]) #入力されたemailからアカウントを1件取得
-#    return if customer.nil? #アカウントを取得できなかった場合、このメソッドを終了
-#    flash[:notice] = "アカウントが見つかりませんでした。"
-#    return unless customer.valid_password?(params[:customer][:password]) #取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了
-#    flash[:notice] = "パスワードが一致しませんでした"
-    
-#    if @customer.active_for_authentication? == true #アカウントがactiveだったら
-#     return # ここでcustomer_stateメソッドの実行を終了し、createアクションを実行
-#    else
-#     redirect_to signup_form_path #ここでサインアップ画面への遷移処理を実行
-#      return
-#    end
-#  end
-  
-  def after_sign_in_path_for(resource) #admin-order-indexへパス変更
-    
-  end
-  
-  def after_sign_out_path_for(resource) #ログイン画面へパス追加
-   admin_root_path
-  end
-
-  
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :encrypted_password])
-  end
-
 end
